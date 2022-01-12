1d347530afd428a9fa3c7dfcf85e53da6534b454
DevMode <devmode@Devs-MacBook-Pro.local>
2022-01-12 14:06:11 +0530

    test after Mac Pro

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from store.models import Product
 from cart.models import Cart, CartItem
@@ -18,7 +18,7 @@ def _cart_id(request):
 
 
 
-
+# To click on the + sign on the quantity to increment the product quantity
 def add_cart(request, product_id):
     product = Product.objects.get(id = product_id)           #get the product using the product's id   
     try:
@@ -44,6 +44,30 @@ def add_cart(request, product_id):
     return redirect('cart')    
 
 
+# To click on the - sign on the quantity to decrement the product quantity and remove it if the quantity becomes 0
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity >1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()    
+    return redirect('cart')    
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+
+
+
 
 def cart(request, total = 0, quantity = 0, cart_items = None):
     try:
@@ -51,13 +75,19 @@ def cart(request, total = 0, quantity = 0, cart_items = None):
         cart_items = CartItem.objects.filter(cart=cart, is_active=True) # we got all the items addd inside the cart
         for cart_item in cart_items:
             total = total+ (cart_item.product.price * cart_item.quantity) #total initially set to 0, and here we are adding Product.price * quantity to get the total amount
-            quantity +=cart_item.qunatity                                 #quantity initialyy set to 0, here we are getting the quntity from the cart 
+            quantity +=cart_item.quantity    #quantity initialyy set to 0, here we are getting the quntity from the cart 
+        
+        gst = (5*total)/100    
+        grand_total = total+gst                           
     except ObjectDoesNotExist:
-        pass        
+        pass        # Just ignore
 
     context = {
         'total': total,
         'quantity': quantity,
-        'cart_items': cart_items 
+        'cart_items': cart_items ,
+        'gst':gst,
+        'grand_total': grand_total,
+
     }
     return render(request, 'cart.html', context)        
